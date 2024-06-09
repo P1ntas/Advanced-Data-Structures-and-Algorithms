c21c02f559884bdeb2276af85dccc78d29a7f950
Malva2312 <up202006605@edu.fe.up.pt>
2024-06-09 18:49:08 +0100

    Added documentation to song.h file

--- KDTree/src/song.h
+++ KDTree/src/song.h
@@ -35,6 +35,7 @@ private:
 public:
     // Destructor
     ~Song() {}
+
     // Constructor
     Song();
     Song(std::map<std::string, double> numeric_data,
@@ -43,24 +44,70 @@ public:
          std::vector<std::string> artists,
          std::string release_date);
 
-    // All operators
+
+    /**
+     * @brief Overloaded less than operator for comparing two Song objects.
+     * @param song The Song object to compare with.
+     * @return True if this Song is less than the given Song, false otherwise.
+     */
     bool operator<(const Song &song) const;
+
+    /**
+     * @brief Overloaded equality operator for comparing two Song objects.
+     * @param song The Song object to compare with.
+     * @return True if this Song is equal to the given Song, false otherwise.
+     */
     bool operator==(const Song &song) const;
+
+    /**
+     * @brief Overloaded inequality operator for comparing two Song objects.
+     * @param song The Song object to compare with.
+     * @return True if this Song is not equal to the given Song, false otherwise.
+     */
     bool operator!=(const Song &song) const;
+
+    /**
+     * @brief Overloaded greater than operator for comparing two Song objects.
+     * @param song The Song object to compare with.
+     * @return True if this Song is greater than the given Song, false otherwise.
+     */
     bool operator>(const Song &song) const;
 
-    // Assigmnet operator
+    /**
+     * @brief Assignment operator for assigning the values of another Song object to this Song object.
+     * @param song The Song object to assign from.
+     * @return A reference to this Song object after assignment.
+     */
     Song &operator=(const Song &song);
 
-    // Coordinate getter
+    /**
+     * @brief Get the coordinates of the Song.
+     * @return A vector of doubles representing the coordinates of the Song.
+     */
     std::vector<double> get_coordinates() const;
-    // Key getter
+
+    /**
+     * @brief Get the keys of the Song.
+     * @return A constant vector of strings representing the keys of the Song.
+     */
     const std::vector<std::string> get_keys() const;
-    // Name getter
+
+    /**
+     * @brief Get the name of the Song.
+     * @return A string representing the name of the Song.
+     */
     std::string get_name() const;
-    // Numeric data getter
+
+    /**
+     * @brief Get the numeric data of the Song.
+     * @return A map of strings to doubles representing the numeric data of the Song.
+     */
     std::map<std::string, double> get_numeric_data() const;
-    // ID getter
+
+    /**
+     * @brief Get the ID of the Song.
+     * @return A string representing the ID of the Song.
+     */
     std::string get_id() const;
 };
 
